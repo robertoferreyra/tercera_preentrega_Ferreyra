a2b193026a496a60fc54f77b4cd09360ee29ce6a
Matias Ferreyra <ferreyra.matt@gmail.com>
2024-07-29 18:39:56 -0300

    termine clase 21

--- matias/matias_app/urls.py
+++ matias/matias_app/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path('cursos/', views.cursos, name= "Cursos"),
     path('profesores/', views.profesores, name= "Profesores"),
     path('estudiantes/', views.estudiantes, name= "Estudiantes"),
-    path('entregables/', views.entregables, name= "Entregables")
+    path('entregables/', views.entregables, name= "Entregables"),
 ]
+
+clase_21= [
+    path('curso-formulario/', views.curso_formulario, name="CursoFormulario"),
+    path('form-con-api/', views.form_con_api, name="FormConApi"),
+    path('buscar-form-con-api/', views.buscar_form_con_api, name="Buscar_Form_Con_Api"),
+]
+
+urlpatterns += clase_21
--- matias/matias_app/views.py
+++ matias/matias_app/views.py
@@ -3,15 +3,63 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from matias_app.models import Curso
+from matias_app.forms import CursoFormulario
+from matias_app.forms import BuscaCursoForm
 
 def inicio(request):
     return render(request, "matias_app/index.html")
 
 def cursos(request):
-    return render(request, "matias_app/cursos.html")
+    return HttpResponse("Vista cursos")
+
 def profesores(request):
-    return render(request, "matias_app/profesores.html")
+    return HttpResponse("Vista profesores")
+
 def estudiantes(request):
-    return render(request, "matias_app/estudiantes.html")
+    return HttpResponse("Vista estudiantes")
+
 def entregables(request):
-    return render(request, "matias_app/entregables.html")
+    return HttpResponse("Vista entregables")
+
+def curso_formulario(request):
+
+    if request.method == 'POST':
+
+        curso = Curso(nombre=request.POST['curso'],camada=request.POST['camada'])
+        curso.save()
+
+        return render(request, "matias_app/index.html")
+
+    return render(request,"matias_app/curso_formulario.html")
+
+
+def form_con_api(request):
+    if request.method == "POST":
+        mi_formulario = CursoFormulario(request.POST) 
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            
+            curso = Curso(nombre=informacion["curso"], camada=informacion["camada"])
+            curso.save()
+
+            return render(request, "matias_app/index.html")
+    else:
+        mi_formulario = CursoFormulario()
+
+    return render(request, "matias_app/form_con_api.html", {"mi_formulario": mi_formulario})
+
+def buscar_form_con_api(request):
+    if request.method == "POST":
+        mi_formulario = BuscaCursoForm(request.POST) 
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            
+            cursos = Curso.objects.filter(nombre__icontains=informacion["curso"])
+
+            return render(request, "matias_app/mostrar_cursos.html", {"cursos": cursos})
+    else:
+        mi_formulario = BuscaCursoForm()
+
+    return render(request, "matias_app/buscar_form_con_api.html", {"mi_formulario": mi_formulario})
