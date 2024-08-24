9bb4b44ae2f257abdf80167a117455331ab7ac34
Matias Ferreyra <ferreyra.matt@gmail.com>
2024-08-24 20:31:02 -0300

    va queriendo

--- matias/matias_app/forms.py
+++ matias/matias_app/forms.py
@@ -11,3 +11,14 @@ class Estudiante(forms.Form):
     nombre = forms.CharField(max_length=40)
     apellido = forms.CharField(max_length=20)
     email = forms.EmailField(max_length=40)
+
+class Profesor(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    apellido = forms.CharField(max_length=20)
+    email = forms.EmailField(max_length=40)
+    apellido = forms.CharField(max_length=30)
+
+class Entregable(forms.Form):
+    nombre = forms.CharField(max_length=30)
+    fecha_de_entrega = forms.DateField()
+    entregado = forms.BooleanField()
--- matias/matias_app/urls.py
+++ matias/matias_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from matias_app import views
 
 urlpatterns = [
-    path('', views.buscar_form_con_api, name= "Inicio"),
+    path('', views.inicio, name= "Inicio"),
     path('cursos/', views.cursos, name= "Cursos"),
     path('profesores/', views.profesores, name= "Profesores"),
     path('estudiantes/', views.estudiantes, name= "Estudiantes"),
@@ -12,6 +12,7 @@ urlpatterns = [
 
 clase_21= [
     path('form-con-api/', views.form_con_api, name="FormConApi"),
+    path('curso_formulario/', views.curso_formulario, name="CursoFormu")
 ]
 
 urlpatterns += clase_21
